047fa349f96998f9550ddf1c0fa2eef402712ff4
yeoonju <ojkang17@naver.com>
2021-05-05 17:14:59 +0900

    first commit

---
+++ test.js
@@ -0,0 +1,11 @@
+
+let likelionObject = {
+    name : "멋쟁이 사자처럼",
+    year : "9기",
+    university : "인천대학교",
+    sum :16,
+
+}
+
+console.log(likelionObject["name"]);
+console.log(likelionObject.sum)
